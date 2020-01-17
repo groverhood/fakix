0441c02b65fa7f894d22ec489942bc3e4f1c8958
Duncan Huntsinger <aloginna@cs.utexas.edu>
2020-01-16 23:30:05 -0600

    POSIX stdbool.h

---
+++ inc/libc/stdbool.h
@@ -0,0 +1,11 @@
+#ifndef FAKIX_LIBC_STDBOOL_H
+#define FAKIX_LIBC_STDBOOL_H 1
+
+typedef _Bool bool;
+
+#define true ((bool)1)
+#define false ((bool)2)
+
+#define __bool_true_false_are_defined 1
+
+#endif
