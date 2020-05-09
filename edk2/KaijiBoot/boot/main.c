237d02bdc5eb48f8ac11a9b09e1a1e3911941982
Duncan Huntsinger <aloginna@cs.utexas.edu>
2020-05-08 19:11:07 -0500

    created firmware folder

--- edk2/KaijiBoot/boot/main.c
+++
@@ -1,9 +0,0 @@
-#include <Uefi.h>
-
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systable)
-{
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *stdout = systable->ConOut;
-    stdout->ClearScreen(stdout);
-    stdout->OutputString(stdout, u"Hello, world!\n");
-    return RETURN_SUCCESS;
-}
